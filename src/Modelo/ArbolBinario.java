706636cd910be85fff2e5ecdb113aa6ba63c1e11
Sofito-code <65092315+Sofito-code@users.noreply.github.com>
2021-09-04 14:36:42 -0500

    Agregar actualizado
    Ahora el método agregar del árbol binario acepta un dato de tipo Object, para decidir si es mayor o menor que algún dato se usa el hashcode()

--- src/Modelo/ArbolBinario.java
+++ src/Modelo/ArbolBinario.java
@@ -4,35 +4,36 @@
  * and open the template in the editor.
  */
 package Modelo;
-import java.util.*;
 
+import java.util.*;
 
 /**
  *
  * @author Sofito-Chan
  */
 public class ArbolBinario {
+
     private NodoDoble raiz;
     public static final int INORDEN = 0;
     public static final int PREORDEN = 1;
     public static final int POSORDEN = 2;
-    private List<NodoDoble>Hojas;
+    private List<NodoDoble> hojas;
 
     public ArbolBinario() {
         raiz = null;
-        Hojas=new ArrayList();
+        hojas = new ArrayList();
     }
-    public ArbolBinario(NodoDoble x) 
-    	{ raiz=x;
-    		Hojas=new ArrayList();
-    	
-    	}
+
+    public ArbolBinario(NodoDoble x) {
+        raiz = x;
+        hojas = new ArrayList();
+    }
 
     public boolean esVacio() {
         return (raiz == null);
     }
 
-    public void agregar(double d) {
+    public void agregar(Object d) {
         NodoDoble n = new NodoDoble(d);
 
         /*
@@ -54,12 +55,12 @@ public class ArbolBinario {
          * dirección de su padre (q).
          */
         while (p != null) {
-            if ((double) p.retornaDato() == d) {
+            if ((Object) p.retornaDato() == d) {
                 System.out.println("Dato ya existe");
                 return;
             }
             q = p;
-            if (d < (double) p.retornaDato()) {
+            if (d.hashCode() < p.retornaDato().hashCode()) {
                 p = p.retornaLI();
             } else {
                 p = p.retornaLD();
@@ -70,7 +71,7 @@ public class ArbolBinario {
          * derecha del nodo p y se procede con la inserción.
          * 
          */
-        if (d > (double) q.retornaDato()) {
+        if (d.hashCode() > q.retornaDato().hashCode()) {
             q.asignaLD(n);
             System.out.println(n.retornaDato() + "Agregado a la derecha de " + q.retornaDato());
         } else {
@@ -178,101 +179,141 @@ public class ArbolBinario {
         }
         int izquierda = alturaPorNodo(x.retornaLI());
         int derecha = alturaPorNodo(x.retornaLD());
-        if (izquierda > derecha){
-            return (izquierda+1);
+        if (izquierda > derecha) {
+            return (izquierda + 1);
         }
-        return (derecha+1);
+        return (derecha + 1);
     }
-    public List<NodoDoble> nodoshoja()
-    { 	 nhr(raiz);
-    	return this.Hojas;
-    	
+
+    public List<NodoDoble> nodoshoja() {
+        nhr(raiz);
+        return this.hojas;
     }
-    public void nhr(NodoDoble x)
-    	{ if(x==null) 
-    		{return;}
-    	  if(x.retornaLD()!=null && x.retornaLI()!=null) 
-    	  	{this.Hojas.add(x);
-    	  		return;
-    	  	}
-    	  else {
-    		  if(x.retornaLD()!=null) {nhr(x.retornaLD());}
-    		  if(x.retornaLI()!=null) {nhr(x.retornaLD());}
-    	  }
-    	  return;
-    	  
-    	}
-    public List<NodoDoble> nhrv2(NodoDoble x)
-    	{ if(x==null) {return null;}
-    		List<NodoDoble> Aux=new ArrayList();
-    		if(x.retornaLD()==null && x.retornaLI()==null) 
-    			{Aux.add(x);}
-    		else {
-    			if(x.retornaLD()!=null) {Aux.addAll(nhrv2(x.retornaLD()));}
-    			if(x.retornaLI()!=null) {Aux.addAll(nhrv2(x.retornaLI()));}
-    		}
-    	return Aux;
-    	}
-    public NodoDoble padre(NodoDoble x) 
-    	{	if (x==raiz) 
-    			{return raiz;}
-    		return pdr(x,raiz);
-    		
-    	}
-    private NodoDoble pdr(NodoDoble x,NodoDoble origen) 
-    	{
-    		if(origen.retornaLD()==null && origen.retornaLI()==null) {return null;}
-    		else {    		
-    			NodoDoble aux1;
-    			NodoDoble aux2; 
-    			if(origen.retornaLD()==x || origen.retornaLI()==x) 
-    			{return origen;}
 
-    		else { if(origen.retornaLD()!=null ) 
-    				{aux1=pdr(x,origen.retornaLD());
-    				if(aux1!=null) {return aux1;}}
-    		 if(origen.retornaLI()!=null ) 
-				{aux1=pdr(x,origen.retornaLI());
-				if(aux1!=null) {return aux1;}}
-    			
-    		}
-    			
-    		}
-    		return null;
-    	}
-    public List<NodoDoble> ancestros(NodoDoble x)
-    	{	List aux=new ArrayList();
-    		NodoDoble pc;
-    		if(x==null) {return null;}
-    		pc=padre(x);
-    		while(pc!=raiz) 
-    			{aux.add(pc);
-    			 pc=padre(pc);}
-    		return aux;
-    	
-    	}
-    public NodoDoble hermano(NodoDoble x) 
-    	{if(x==null) {return null;}
-    	 NodoDoble aux=padre(x);
-    	 if (aux.retornaLD()!=null && aux.retornaLD()!=x) {return aux.retornaLD();}
-    	 if (aux.retornaLI()!=null && aux.retornaLI()!=x) {return aux.retornaLI();}
-    	 return  null;
-    	}
-    public int esIzorDe(NodoDoble x) 
+    public void nhr(NodoDoble x) {
+        if (x == null) {
+            return;
+        }
+        if (x.retornaLD() != null && x.retornaLI() != null) {
+            this.hojas.add(x);
+            return;
+        } else {
+            if (x.retornaLD() != null) {
+                nhr(x.retornaLD());
+            }
+            if (x.retornaLI() != null) {
+                nhr(x.retornaLD());
+            }
+        }
+        return;
+    }
 
-    	{ if(x==null) {return -1;}
-    	  NodoDoble aux=padre(x);
-    	  if(aux.retornaLD()==x) {return 1;}
-    	  if(aux.retornaLI()==x) {return 0;}    	  
-    	  return -1;
-    	
-    	}
+    public List<NodoDoble> nhrv2(NodoDoble x) {
+        if (x == null) {
+            return null;
+        }
+        List<NodoDoble> Aux = new ArrayList();
+        if (x.retornaLD() == null && x.retornaLI() == null) {
+            Aux.add(x);
+        } else {
+            if (x.retornaLD() != null) {
+                Aux.addAll(nhrv2(x.retornaLD()));
+            }
+            if (x.retornaLI() != null) {
+                Aux.addAll(nhrv2(x.retornaLI()));
+            }
+        }
+        return Aux;
+    }
+
+    public NodoDoble padre(NodoDoble x) {
+        if (x == raiz) {
+            return raiz;
+        }
+        return pdr(x, raiz);
+    }
+
+    private NodoDoble pdr(NodoDoble x, NodoDoble origen) {
+        if (origen.retornaLD() == null && origen.retornaLI() == null) {
+            return null;
+        } else {
+            NodoDoble aux1;
+            NodoDoble aux2;
+            if (origen.retornaLD() == x || origen.retornaLI() == x) {
+                return origen;
+            } else {
+                if (origen.retornaLD() != null) {
+                    aux1 = pdr(x, origen.retornaLD());
+                    if (aux1 != null) {
+                        return aux1;
+                    }
+                }
+                if (origen.retornaLI() != null) {
+                    aux1 = pdr(x, origen.retornaLI());
+                    if (aux1 != null) {
+                        return aux1;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    public List<NodoDoble> ancestros(NodoDoble x) {
+        List aux = new ArrayList();
+        NodoDoble pc;
+        if (x == null) {
+            return null;
+        }
+        pc = padre(x);
+        while (pc != raiz) {
+            aux.add(pc);
+            pc = padre(pc);
+        }
+        return aux;
+
+    }
+
+    public NodoDoble hermano(NodoDoble x) {
+        if (x == null) {
+            return null;
+        }
+        NodoDoble aux = padre(x);
+        if (aux.retornaLD() != null && aux.retornaLD() != x) {
+            return aux.retornaLD();
+        }
+        if (aux.retornaLI() != null && aux.retornaLI() != x) {
+            return aux.retornaLI();
+        }
+        return null;
+    }
+
+    public int esIzorDe(NodoDoble x) {
+        if (x == null) {
+            return -1;
+        }
+        NodoDoble aux = padre(x);
+        if (aux.retornaLD() == x) {
+            return 1;
+        }
+        if (aux.retornaLI() == x) {
+            return 0;
+        }
+        return -1;
+
+    }
+
     public NodoDoble tio(NodoDoble x) {
-    	if(x==null) {return null;}
-    	return hermano(padre(x));}
-    public NodoDoble abuelo(NodoDoble x)
-    {
-    	if(x==null) {return null;}
-    	return padre(padre(x));	
+        if (x == null) {
+            return null;
+        }
+        return hermano(padre(x));
     }
+
+    public NodoDoble abuelo(NodoDoble x) {
+        if (x == null) {
+            return null;
+        }
+        return padre(padre(x));
     }
+}
